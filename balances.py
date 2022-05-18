82a39811b3ab0dd3bf33769baff8a927faeff8f0
Samuel Price <samjprice@yahoo.co.uk>
2022-05-18 10:40:16 -0500

    loop added and adjusted for dynamic data

--- balances.py
+++ balances.py
@@ -13,20 +13,30 @@ def currency_panda(row):
 
 def balances():
     # change directory and open file
-    os.chdir('./csv_data/cash')
-    df = pd.read_csv('Starling.csv',)
+    dir = os.path.join('csv_data','cash')
+    for x in os.listdir(dir):
+        print(x)
+        if os.path.isdir(os.path.join(dir, x)):
 
-    # drop duplicate dates, keeping last balance, dropping unnecessary columns
-    df = df.drop_duplicates(subset=["date"], keep="last").reset_index()
-    df = df.reindex(columns = ['date','currency','balance'])
+            try:
+                df = pd.read_csv(os.path.join(dir,x+ '.csv'))
+            except FileNotFoundError:
+                print(x+ '.csv does not exist')
+                continue
+            except Exception as e:
+                print(e)
+                continue
 
-    df["starling"] = df.apply(currency_panda, axis=1)
-    df = df.drop(columns = ['currency','balance'])
+            # drop duplicate dates, keeping last balance, dropping unnecessary columns
+            df = df.drop_duplicates(subset=["date"], keep="last").reset_index()
+            df = df.reindex(columns = ['date','currency','balance'])
 
-    df.set_index('date', inplace=True)
+            df[x] = df.apply(currency_panda, axis=1)
+            df = df.drop(columns = ['currency','balance'])
 
-    df.to_csv('starling_balance.csv')
+            df.set_index('date', inplace=True)
+
+            df.to_csv(os.path.join('csv_data','balances',x + '_balance.csv'))
 
 if __name__ == '__main__':
     balances()
-    pass
