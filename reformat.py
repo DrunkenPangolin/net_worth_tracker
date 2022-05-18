3267ae482973f61a72bd98659759562ea6f5f84a
Samuel Price <samjprice@yahoo.co.uk>
2022-05-18 09:44:23 -0500

    removed chdir, added try/except

--- reformat.py
+++ reformat.py
@@ -13,18 +13,18 @@ def reformat():
         "description",
         "type",
         "amount",
-        "currency",
         "balance",
+        "currency",
         "category",
     ]
 
     # get name of folder in account directory, and access it
-    os.chdir(os.path.join(".", "csv_data",'cash'))
-    for account_name in os.listdir('.'):
-        if ".csv" not in account_name:
+    dir = os.path.join(".", "csv_data",'cash')
+    for account_name in os.listdir(dir):
+        if os.path.isdir(os.path.join(dir, account_name)):
             print(account_name)
 
-            filepath = os.path.join('.', account_name)
+            filepath = os.path.join(dir, account_name)
             file_list = os.listdir(filepath)
 
             # account specific reformat to csv (if required)
@@ -37,18 +37,22 @@ def reformat():
                 continue
 
             # dropping columns according to account based function
-            df = globals()[account_name.lower().replace(" ", "_")](df)      # to be imported from accounts.py
+            try:
+                df = globals()[account_name.lower().replace(" ", "_")](df)      # to be imported from accounts.py
+            except Exception as e:
+                print(str(e) + " function not found")
+                continue
 
             if 'time' not in list(df.columns):
                 df['time'] = df.index
             df["date"] = pd.to_datetime(df.date, dayfirst=True)             # converting to date object, reading dd/mm/yyyy
+            df['currency'] = np.where(df['currency'] == np.nan, accounts().loc[account_name,'currency'], df['currency'])
             df = df.sort_values(['date','time']).reset_index()              # order by date, new index
             df = df.reindex(columns = new_columns)                          # adds & removes columns to create uniform output
             df.set_index('date', inplace=True)
-
-            df.to_csv(account_name + ".csv")                                # write to file
-    os.chdir('..')
-    os.chdir('..')
+            print(df)
+            df.to_csv(os.path.join(dir,account_name + ".csv"))              # write to file
+ 
 
 
 if __name__ == "__main__":
