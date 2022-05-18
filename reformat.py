0232bec3c2bc6aa1123be14f346f0e7d46550296
Samuel Price <samjprice@yahoo.co.uk>
2022-05-17 23:24:41 -0500

    Update reformat.py
    removed accounts functions to accounts.py and set import and app_name

--- reformat.py
+++ reformat.py
@@ -1,95 +1,9 @@
 import numpy as np
 import os
 import pandas as pd
+from accounts import *
 
-
-def change_cat(df: pd.DataFrame, new_category: str, column: str, criteria: str) -> pd.DataFrame:
-    df["category"] = np.where(df[column].str.contains(criteria), new_category, df["category"])
-
-
-def paypal(df: pd.DataFrame):
-    df.columns = [
-        "date",
-        "time",
-        "time_zone",
-        "name",
-        "type",
-        "status",
-        "currency",
-        "gross",
-        "fee",
-        "net",
-        "receipt_id",
-        "balance",
-        "tip",
-    ]
-    return df
-
-
-def premium_bonds(df: pd.DataFrame):
-    df.columns = [
-        "date",
-        "description",
-        "amount",
-        "balance",
-        "action"
-    ]
-    df["category"] = "internal"
-    df["currency"] = "GBP"
-
-    change_cat(df, "interest", "description", "prize")
-    df["amount"] = np.where(
-        df["description"].str.contains("deposit|prize"),
-        df["amount"],
-        df["amount"] * (-1),
-    )
-    df = df.sort_index(ascending=False).reset_index()
-    return df
-
-
-def starling(df: pd.DataFrame):
-    df.columns = [
-        "date",
-        "name",
-        "description",
-        "type",
-        "amount",
-        "balance",
-        "category",
-        "notes",
-    ]
-    df["currency"] = "GBP"
-
-
-    # changing categories
-    change_cat(df, "investments", "name",
-        """
-        Vanguard|
-        AJ Bell|
-        Freetrade|
-        212|
-        Halifax Share Dealing
-        """,
-    )
-    change_cat(df, "internal", "name",
-        """
-        Chase|
-        Virgin Money|
-        TSB|
-        Royal Bank of Scotland|
-        Tandem|
-        Me -|
-        Samuel Price|
-        Sainsbury's Bank|
-        Revolut|
-        New Day|
-        Nationwide|
-        Halifax Cards
-        """
-    )
-    change_cat(df, "interest", "description", "nterest")
-    return df
-
+APP_NAME = "csv_reformatter"
 
 def reformat():
     # get name of folder in account directory, and access it
@@ -111,7 +25,7 @@ def reformat():
                 continue
 
             # dropping columns according to account based function
-            df = globals()[account_name.lower().replace(" ", "_")](df)
+            df = globals()[account_name.lower().replace(" ", "_")](df)      # to be imported from accounts.py
 
             new_columns = [
                 "date",
