786f02f65bcb7aa5aa7917016b9ef54329bfbd7a
Samuel Price <samjprice@yahoo.co.uk>
2022-07-03 21:51:28 +0200

    layout and dashboard files

--- flask/app.py
+++ flask/app.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 
 
 @app.route("/")
-@app.route("/index")
+@app.route("/dashboard")
 def index():
-    return render_template("index.html", title="Index")
+    return render_template("dashboard.html", title="Dashboard")
 
 if __name__ == "__main__":
     app.run(debug=True)
